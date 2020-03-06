0bd5d5e9d605c865a033b7e77a48f39c4ac179f9
fukuchan <fukuchan@fukuchan.mynetgear.com>
2020-03-06 10:02:00 +0900

    Initial commit

---
+++ qiita-link-generation.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Qiita記事リンク生成
+// @version      0.1
+// @description  Qiita記事へのリンクをMarkdown記法で生成する
+// @author       fukuchan
+// @match        https://qiita.com/*/items/*
+// ==/UserScript==
+
+const span = document.createElement("span");
+span.classList.add("fa", "fa-link");
+
+const div = document.createElement("div");
+div.classList.add("it-Actions_shareButton");
+div.append(span);
+div.addEventListener("click", () => prompt("", `[${document.title}](${location.href})`));
+
+const container = document.querySelector(".it-Actions");
+container.append(div);
